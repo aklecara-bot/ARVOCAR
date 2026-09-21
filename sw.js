f42910385c04aadcac7a095ea0ed2e73001f4dc6
Fabricio Rocha Teixeira <115423686+FabricioOnnil@users.noreply.github.com>
2026-09-21 15:34:13 -0300

    add

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // =========================================================================
 // SERVICE WORKER - PWA OFFLINE CACHE
 // =========================================================================
-const CACHE_NAME = 'arvo-mobile-v1.10';
+const CACHE_NAME = 'arvo-mobile-v1.11';
 
 const ASSETS_TO_CACHE = [
   '/arvocarmobile/frontendmobile/mobile.html',
